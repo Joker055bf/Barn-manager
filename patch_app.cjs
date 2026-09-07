b8de8d5d28b4228767ff5f3acd8fe34151b10923
Joker055bf <Joker055bf@users.noreply.github.com>
2026-09-07 21:58:32 +0300

    Clean temp patch files

--- patch_app.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-let lines = fs.readFileSync('App.tsx', 'utf8').split('\n');
-console.log(lines.slice(3535, 3565).map((l, i) => `${3536 + i}: ${JSON.stringify(l)}`).join('\n'));
